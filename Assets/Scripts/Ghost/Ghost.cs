f8420045313a0f1fcda74ac2eea916d69eb955db
Mateotorija <mateotorija@gmail.com>
2024-04-22 21:28:33 -0300

    ataque enemigo (no funciona)

--- Assets/Scripts/Ghost/Ghost.cs
+++ Assets/Scripts/Ghost/Ghost.cs
@@ -6,13 +6,24 @@ using UnityEngine;
 public class Ghost : Player
 {
     [SerializeField] int life;
+    [SerializeField] float fuerzaEmpuje;
     public float attackCooldown;
     public Action onAttack = delegate { };
     Coroutine _cooldown;
+    [SerializeField] Ghost _model;
     public void Attack()
     {
         _cooldown = StartCoroutine(Cooldown());
         onAttack();
+        //_model.transform.position += transform.forward * Time.deltaTime * fuerzaEmpuje;
+
+        //version 2
+
+        //Vector3 direccionOpuesta = _model.transform.position - transform.position;
+        //direccionOpuesta.Normalize();
+
+        //Rigidbody personajeRigidbody = _model.GetComponent<Rigidbody>();
+        //personajeRigidbody.AddForce(direccionOpuesta * fuerzaEmpuje);
     }
     IEnumerator Cooldown()
     {
